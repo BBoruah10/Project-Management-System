c6e104c01dde6e5dc18e61485a1dd1422d5154d0
bhaskar <baruahbhaskar10@gmail.com>
2025-03-07 23:31:37 +0530

    Added reduxjs toolkit

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,20 +1,33 @@
+"use-client";
+
 import React from "react";
 import Navbar from "@/app/(components)/Navbar";
 import Sidebar from "@/app/(components)/Sidebar";
-
-const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+import StoreProvider, { useAppSelector } from "./redux";
+//to access GLOBAL state in the layout-->to use the state we have to pass the DashboardLayout in the StoreProvider
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  );
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
       {/*sidebar-->flex*/}
       <Sidebar />
       <main
-        className={`dark:bg-dark-bg flex w-full flex-col bg-gray-50 md:pl-64`}
+        className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}
       >
         {/*navbar-->content*/}
         <Navbar />
         {children}
       </main>
     </div>
+  );
+};
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
   );
 };
 
