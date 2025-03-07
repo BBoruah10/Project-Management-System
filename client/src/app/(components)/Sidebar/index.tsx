6f70d695258befa33e7856eef3541eab7e149b29
bhaskar <baruahbhaskar10@gmail.com>
2025-03-07 12:31:35 +0530

    added the images to public

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { LockIcon } from "lucide-react";
+import Image from "next/image";
 import React, { useState } from "react";
 
 const Sidebar = () => {
@@ -12,6 +14,19 @@ const Sidebar = () => {
         <div className="min-h-[56px]w-64 z-50 flex items-center justify-between bg-white px-6 pt-3 dark:bg-black">
           <div className="darh:text-white text-xl font-bold text-gray-800">
             Manage
+          </div>
+        </div>
+        {/*TEAM*/}
+        <div className="border-y=[1.5px] flex items-center gap-5 border-gray-200 px-8 py-4 dark:border-gray-700">
+          <Image src="/logo.png" alt="logo" width={40} height={40} />
+          <div>
+            <h3 className="text-md font-bold tracking-wide dark:text-gray-200">
+              MANAGE TEAM
+            </h3>
+            <div className="mt-1 flex items-start gap-2">
+              <LockIcon className="mt-[0.1rem] h-3 w-3 text-gray-500 dark:text-gray-400" />
+              <p className="text-xs text-gray-500">Private</p>
+            </div>
           </div>
         </div>
       </div>
